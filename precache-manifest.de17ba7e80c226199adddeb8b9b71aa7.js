f5cbd3a4c39e7e1a318924691b6815dc55bc56b6
Lucas Bertoldo <lucas.fber@gmail.com>
2019-03-09 20:16:59 -0300

    Updates

---
+++ precache-manifest.de17ba7e80c226199adddeb8b9b71aa7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a01562efe353b012ba2b",
+    "url": "/gh-test/static/js/runtime~main.f7267332.js"
+  },
+  {
+    "revision": "0eb5dee4081a07f0d9bf",
+    "url": "/gh-test/static/js/main.5ea2618e.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/gh-test/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "0eb5dee4081a07f0d9bf",
+    "url": "/gh-test/static/css/main.f6c2b65c.chunk.css"
+  },
+  {
+    "revision": "90fef0d1816fed2d7c7e99261d3fb532",
+    "url": "/gh-test/index.html"
+  }
+];
